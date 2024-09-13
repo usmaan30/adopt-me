e382bdc49e23d03b49f107d6331349648eb9b1fe
Usman Munir <usmanmunir1008@gmail.com>
2024-09-13 21:10:04 +0500

    made changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "src", // Keep src as the root
   plugins: [react()],
-  root: "src",
+  build: {
+    outDir: "../dist", // Output to the root-level 'dist' directory
+    emptyOutDir: true, // Ensure the output directory is cleaned before building
+  },
 });
